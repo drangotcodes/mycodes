4dbf84da8843b68b3d5f62097fbb998fd89f61f1
dran.coder <191768775+DrianB@users.noreply.github.com>
2024-12-22 18:34:45 +0800

    push

---
+++ activity4.py
@@ -0,0 +1,7 @@
+#fahrenheit top celcius converter
+
+fah = eval(input("Enter a temperature in FAHRENHEIT: "))
+c = ((fah - 32) * 5) / 9
+round(c, 2)
+print(round)
+print(f"The degrees in faherenehit converterd to celcius is {c} ")
