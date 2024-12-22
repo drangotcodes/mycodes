e7396b86dfa668b443b1cbd3ca3c395bae22c646
dran.coder <191768775+DrianB@users.noreply.github.com>
2024-12-22 18:27:07 +0800

    dec 22

---
+++ activity3.py
@@ -0,0 +1,6 @@
+#String formatting
+
+name = input("Enter your name: ")
+loc = input("Enter our address: ")
+age = input("Enter your age: ")
+print(f"\n\t\t\t Hello {name}, from {loc}, age of {age} nice top meet you!")
